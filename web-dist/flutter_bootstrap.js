0498299f521dd5a86da52c2fb584ec48e1a46c3d
it_chan0 <it_chan0@hotmail.com>
2026-06-18 13:13:48 -0600

    chore: update web build

--- web-dist/flutter_bootstrap.js
+++ web-dist/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2715251622" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "136176419" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
