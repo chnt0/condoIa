4cfd97fe8e494931ce497504e28c6eaf90903e26
Juan Andrade <j.andrade@compass.com>
2026-09-09 00:28:15 -0600

    cambio que ahora si jala

--- lib/features/paquetes/models/residente_basico.dart
+++ lib/features/paquetes/models/residente_basico.dart
@@ -7,11 +7,15 @@ class ResidenteBasico {
   final int id;
   final String nombreCompleto;
   final String? unidadHabitacional;
+  final String? telefono;
+  final String? telefono2;
 
   ResidenteBasico({
     required this.id,
     required this.nombreCompleto,
     this.unidadHabitacional,
+    this.telefono,
+    this.telefono2,
   });
 
   factory ResidenteBasico.fromJson(Map<String, dynamic> json) =>
--- lib/features/paquetes/models/residente_basico.g.dart
+++ lib/features/paquetes/models/residente_basico.g.dart
@@ -7,6 +7,8 @@ ResidenteBasico _$ResidenteBasicoFromJson(Map<String, dynamic> json) =>
       id: (json['id'] as num).toInt(),
       nombreCompleto: json['nombreCompleto'] as String,
       unidadHabitacional: json['unidadHabitacional'] as String?,
+      telefono: json['telefono'] as String?,
+      telefono2: json['telefono2'] as String?,
     );
 
 Map<String, dynamic> _$ResidenteBasicoToJson(ResidenteBasico instance) =>
@@ -14,4 +16,6 @@ Map<String, dynamic> _$ResidenteBasicoToJson(ResidenteBasico instance) =>
       'id': instance.id,
       'nombreCompleto': instance.nombreCompleto,
       'unidadHabitacional': instance.unidadHabitacional,
+      'telefono': instance.telefono,
+      'telefono2': instance.telefono2,
     };
